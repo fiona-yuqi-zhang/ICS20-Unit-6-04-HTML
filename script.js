2e61f9c881716e1b1e6fe3c20ee14811413e006b
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-06 15:33:43 +0000

    index,script

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let date=""
-let age=""
+let date = ""
+let age = ""
 function number() {
-date=document.getElementById("date").value
-age=document.getElementById("age").value
-if((date=="Tuesday"||date=="Thursday")||(age>12 && age<21)){
-  alert("you are eligible for student pricing!");
+date = document.getElementById('date').value
+age = document.getElementById('age').value
+if((date == 'Tuesday'||date == 'Thursday')||(age>12 && age<21)){
+  alert('you are eligible for student pricing!');
 }else{
-  alert("You have to paid the price")
+  alert('You have to paid the price')
 }
 }
